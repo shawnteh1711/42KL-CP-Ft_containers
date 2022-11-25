6854c4e284cba388c08ef7d2c310ef36ef877537
Chuah Tse Yung <chuahtseyung2002@gmail.com>
2022-11-25 15:51:53 +0800

    Refractor and added notes for vector.hpp

--- ft_containers/srcs/vector.hpp
+++ ft_containers/srcs/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 12:37:58 by schuah            #+#    #+#             */
-/*   Updated: 2022/11/22 14:24:54 by schuah           ###   ########.fr       */
+/*   Updated: 2022/11/25 15:51:24 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 
 # include "ft_containers.hpp"
 # include "type_traits.hpp"
+# include "iterator.hpp"
 
 /**
  * https://en.cppreference.com/w/cpp/container/vector
- * https://cplusplus.com/reference/vector/vector/vector/
  */
 namespace ft
 {
@@ -27,16 +27,25 @@ namespace ft
 	class vector
 	{
 		public:
-			typedef T                                        value_type;
-			typedef Allocator                                allocator_type;
-			typedef typename allocator_type::size_type       size_type;
-			typedef typename allocator_type::difference_type difference_type;
-			typedef value_type&                              reference;
-			typedef const value_type&                        const_reference;
-			typedef typename allocator_type::pointer         pointer;
-			typedef typename allocator_type::const_pointer   const_pointer;
+			typedef T											value_type;
+			typedef Allocator									allocator_type;
+			typedef typename allocator_type::size_type			size_type;
+			typedef typename allocator_type::difference_type	difference_type;
+			typedef value_type&									reference;
+			typedef const value_type&							const_reference;
+			typedef typename allocator_type::pointer			pointer;
+			typedef typename allocator_type::const_pointer		const_pointer;
 			
 			/* Default constructor */
+			vector()
+			{
+				this->_alloc = allocator_type();
+				this->_start = NULL;
+				this->_end = NULL;
+				this->_capacity = NULL;
+			}
+
+			/* Constructs an empty container with the given allocator */
 			explicit vector(const allocator_type& alloc)
 			{
 				this->_alloc = alloc;
@@ -45,23 +54,23 @@ namespace ft
 				this->_capacity = NULL;
 			}
 			
-			/* Fill constructor */
-			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
+			/* Constructs the container with count copies of elements with value */
+			explicit vector (size_type count, const value_type& value = value_type(), const allocator_type& alloc = allocator_type())
 			{
-				if (n <= 0)
+				if (count <= 0)
 					return ;
-				else if (n > std::min(this->_alloc.max_size(), static_cast<size_type>(std::numeric_limits<difference_type>::max())))
+				else if (count > std::min(this->_alloc.max_size(), static_cast<size_type>(std::numeric_limits<difference_type>::max())))
 					throw std::length_error("Length error");
 				this->_alloc = alloc;
-				this->_start = this->_alloc.allocate(n);
-				this->_capacity = this->_start + n;
+				this->_start = this->_alloc.allocate(count);
+				this->_capacity = this->_start + count;
 				this->_end = _capacity;
-				construct_with_val(this->_start, this->_end, val);
+				construct_with_val(this->_start, this->_end, value);
 			}
 
-			/* Range constructor */
-			template <class InputIterator>
-			vector (InputIterator first, typename enable_if<!is_integral<InputIterator>::value, InputIterator>::type last, const allocator_type& alloc = allocator_type())
+			/* Constructs the container with the contents of the range */
+			template <class InputIt>
+			vector (InputIt first, typename enable_if<!is_integral<InputIt>::value, InputIt>::type last, const allocator_type& alloc = allocator_type())
 			{
 				this->_alloc = alloc;
 				this->_start = NULL;
@@ -72,15 +81,15 @@ namespace ft
 			}
 
 			/* Copy constructor */
-			vector (const vector& x)
+			vector (const vector& other)
 			{
-				this->_alloc = x._alloc;
-				size_type	src_cap = x.capacity();
+				this->_alloc = other._alloc;
+				size_type	src_cap = other.capacity();
 				if (src_cap <= 0)
 					return ;
 				this->_start = this->_alloc.allocate(src_cap);
 				this->_capacity = this->_start + src_cap;
-				this->_end = contruct_from_start(this->_start, x._start, x._end);
+				this->_end = contruct_from_start(this->_start, other._start, other._end);
 			}
 
 			/* Deconstructor */
@@ -93,6 +102,37 @@ namespace ft
 				}
 			}
 
+			/**
+			 * MEMBER FUNCTION
+			 * 	assign
+			 * 	get_allocator
+			 */
+
+			/**
+			 * ELEMENT ACCESS
+			 * 	at
+			 * 	operator[]
+			 * 	front
+			 * 	back
+			 * 	data 
+			 */
+
+			/**
+			 * ITERATORS
+			 * 	begin
+			 * 	end
+			 * 	rbegin
+			 * 	rend 
+			 */
+
+			/**
+			 * CAPACITY
+			 * 	empty
+			 * 	size
+			 * 	max_size
+			 * 	reserve
+			 */
+
 			/*
 			 * Capacity member function. Returns the number of elements that can
 			 * be held in currently allocated storage
@@ -102,7 +142,19 @@ namespace ft
 				return (static_cast<size_type>(this->_end - this->_start));
 			}
 
-			/* Range constructor helper function (Using src as value) */
+			/**
+			 * MODIFIERS
+			 * 	clear
+			 *	insert
+			 *	erase
+			 *	push_back
+			 *	pop_back
+			 *	resize
+			 *	swap 
+			 */
+		
+		private:
+			/* Helper function: Range constructor (Using src as value) */
 			template <typename Iterator>
 			pointer	contruct_from_start(pointer dst, Iterator start, Iterator end)
 			{
@@ -115,7 +167,7 @@ namespace ft
 				return (dst);
 			}
 
-			/* Range constructor helper function (Using val as value) */
+			/* Helper function: Range constructor (Using val as value) */
 			pointer	construct_with_val(pointer dst, const_pointer end, const_reference val)
 			{
 				while (dst != end)
@@ -125,9 +177,8 @@ namespace ft
 				}
 				return (dst);
 			}
-		
-		private:
-			/* Destroys allocated memory from the start */
+
+			/* Helper function: Destroys allocated memory from the start */
 			void	destroy_from_start(pointer start)
 			{
 				for (pointer cur = start; cur != this->_end; cur++)
@@ -141,6 +192,18 @@ namespace ft
 			pointer			_end;
 			pointer			_capacity;
 	};
+
+	/**
+	 * NON-MEMBER FUNCTIONS
+	 * 	opeartor==
+	 * 	opeartor!=
+	 * 	opeartor<
+	 * 	opeartor<=
+	 * 	opeartor>
+	 * 	opeartor>=
+	 * 
+	 * 	std::swap
+	 */
 }
 
 #endif
