8c2f204d78f415c7d771b4b3b2b3f5db9e676581
Chuah Tse Yung <chuahtseyung2002@gmail.com>
2022-12-12 18:03:17 +0800

    Added value type comparison class

--- ft_containers/srcs/map.hpp
+++ ft_containers/srcs/map.hpp
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 12:37:49 by schuah            #+#    #+#             */
-/*   Updated: 2022/12/12 17:27:55 by schuah           ###   ########.fr       */
+/*   Updated: 2022/12/12 18:02:51 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
  */
 namespace ft
 {
-	/* Map comparison function */
+	/* Value type comparison class */
 	template <class Key, class T, class Compare>
 	class vt_compare : public std::binary_function<Key, Key, bool>
 	{
@@ -28,8 +28,68 @@ namespace ft
 			bool	result_type;
 			Key		first_argument_type;
 			Key		second_argument_type;
+			
+			/* Default constructor */
+			vt_compare()
+			{
+			}
+
+			/* Copy constructor */
+			vt_compare(const Compare& c) : _comp(c)
+			{
+			}
+
+			/* Destructor */
+			~vt_compare()
+			{
+			}
+
+			/* Copy assignation operator */
+			vt_compare	&operator=(const vt_compare& other)
+			{
+				this->_comp = other._comp;
+				return (*this);
+			}
+
+			/* Function object */
+			const	Compare& key_comp() const
+			{
+				return (this->_comp);
+			}
+
+			/* Comparisons using key_comp */
+			bool	operator()(const T& x, const T& y) const
+			{
+				return (key_comp()(x.first, y.first));
+			}
+
+			bool	operator()(const Key& x, const T& y) const
+			{
+				return (key_comp()(x, y.first));
+			}
+			
+			bool	operator()(const T& x, const Key& y) const
+			{
+				return (key_comp()(x.first, y));
+			}
+
+			/* Swaps the value of _comp */
+			void	swap(vt_compare& other)
+			{
+				std::swap(this->_comp, other._comp);
+			}
+
 		private:
+			/* Private member variables */
+			Compare	_comp;
+	};
+
+	template <class Key, class T, class Compare>
+	void	swap(vt_compare<Key, T, Compare>& x, vt_compare<Key, T, Compare>& y)
+	{
+		x.swap(y);
 	}
+	
 }
 
 #endif
