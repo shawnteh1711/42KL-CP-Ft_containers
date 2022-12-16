c8931a93dff10be4906abec85277aef3377722bc
Chuah Tse Yung <chuahtseyung2002@gmail.com>
2022-12-16 14:45:00 +0800

    Added const_tree_iterator

--- ft_containers/srcs/rbtree_iterator.hpp
+++ ft_containers/srcs/rbtree_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 13:06:52 by schuah            #+#    #+#             */
-/*   Updated: 2022/12/16 14:07:58 by schuah           ###   ########.fr       */
+/*   Updated: 2022/12/16 14:42:51 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,11 +49,11 @@ namespace ft
 				return (*this);
 			};
 
-			/* Constructor with leaf_node_pointer */
-			tree_iterator(leaf_node_pointer pointer) : ptr(pointer) {};
-
 			/* Constructor with node_pointer */
 			tree_iterator(node_pointer pointer) : ptr(static_cast<leaf_node_pointer>(pointer)) {};
+
+			/* Constructor with leaf_node_pointer */
+			tree_iterator(leaf_node_pointer pointer) : ptr(pointer) {};
 
 			/* Base member function. Accesses the underlying pointer */
 			leaf_node_pointer&	base()
@@ -134,7 +134,117 @@ namespace ft
 		private:
 			leaf_node_pointer	ptr;
 	};
-}
 
+	template <class Key, class T>
+	class	const_tree_iterator
+	{
+		public:
+			typedef std::bidirectional_iterator_tag					iterator_category;
+			typedef Key												value_type;
+			typedef const Key&										reference;
+			typedef const Key*										pointer;
+			typedef T												difference_type;
+			typedef tree_iterator<Key, T>							iterator;
+			typedef typename rbt_node_types<Key>::leaf_node_pointer	leaf_node_pointer;
+			typedef typename rbt_node_types<Key>::node_pointer		node_pointer;
+
+			/* Orthodox canonical form */
+			const_tree_iterator() : ptr(NULL) {};
+			const_tree_iterator(const const_tree_iterator &other) : ptr(other.ptr) {};
+			~const_tree_iterator() {};
+			const_tree_iterator	&operator=(const const_tree_iterator& other)
+			{
+				if (this == &other)
+					return (*this);
+				this->ptr = other.ptr;
+				return (*this);
+			};
+
+			/* Constructor with node_pointer */
+			const_tree_iterator(node_pointer pointer) : ptr(static_cast<leaf_node_pointer>(pointer)) {};
+
+			/* Constructor with iterator */
+			const_tree_iterator(iterator it) : ptr(it.base()) {};
+
+			/* Base member function. Accesses the underlying pointer */
+			leaf_node_pointer&	base()
+			{
+				return (this->ptr);
+			};
+
+			const leaf_node_pointer&	base() const
+			{
+				return (this->ptr);
+			};
+
+			/* Returns leaf_node_pointer in node_pointer type */
+			node_pointer node_ptr() const
+			{
+				return (static_cast<node_pointer>(this->ptr));
+			};
+
+			/* Accesses the pointed-to element */
+			reference	operator*() const
+			{
+				return (static_cast<node_pointer>(this->ptr)->_value);
+			};
+
+			pointer	operator->() const
+			{
+				return (&(operator*()));
+			};
+
+			/* Advances of decrements the iterator */
+			const_tree_iterator&	operator++()
+			{
+				ptr = iter_to_next<leaf_node_pointer>(static_cast<node_pointer>(this->ptr));
+				return (*this);
+			};
+
+			const_tree_iterator	operator++(int)
+			{
+				const_tree_iterator	it = *this;
+				++(*this);
+				return (it);
+			}
+
+			const_tree_iterator&	operator--()
+			{
+				ptr = iter_to_prev<node_pointer>(ptr);
+				return (*this);
+			}
+
+			const_tree_iterator	operator--(int)
+			{
+				const_tree_iterator	it = *this;
+				--(*this);
+				return (it);
+			}
+
+			/* Compares the underlying pointer */
+			bool	operator==(const const_tree_iterator& other) const
+			{
+				return (ptr == other.ptr);
+			}
+
+			bool	operator==(const iterator& other) const
+			{
+				return (ptr == other.base());
+			}
+
+			bool	operator!=(const const_tree_iterator& other) const
+			{
+				return (!(*this == other));
+			}
+
+			bool	operator!=(const iterator& other) const
+			{
+				return (!(*this == other));
+			}
+
+		private:
+			leaf_node_pointer	ptr;
+	};
+}
 
 #endif
